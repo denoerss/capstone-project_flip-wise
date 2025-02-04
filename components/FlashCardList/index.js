98dfa6a1ba74206c71f4623a3b1e9f3568d63486
Joshua Nuckel <joshuanuckel@icloud.com>
2025-02-04 16:22:50 +0100

    make each card toggle individually

--- components/FlashCardList/index.js
+++ components/FlashCardList/index.js
@@ -1,7 +1,6 @@
 import { flashcards } from "@/lib/data";
-import { collections } from "@/lib/data";
+import FlashCard from "../FlashCard";
 import styled from "styled-components";
-import { useState } from "react";
 
 const StyledList = styled.ul`
   display: flex;
@@ -12,28 +11,6 @@ const StyledList = styled.ul`
   padding-left: 0px;
 `;
 
-const StyledCard = styled.li`
-  background-color: lightgray;
-  list-style: none;
-  width: 80%;
-  border-radius: 20px;
-  padding: 25px 25px;
-  line-height: 1.25;
-  &:hover {
-    cursor: pointer;
-  }
-`;
-
-const StyledQuestion = styled.p`
-  font-size: 1.75rem;
-  font-weight: 600;
-`;
-
-const StyledAnswer = styled.p`
-  font-size: 1.75rem;
-  font-weight: 400;
-`;
-
 const StyledHeading = styled.h2`
   text-align: center;
   font-weight: bold;
@@ -41,30 +18,17 @@ const StyledHeading = styled.h2`
 `;
 
 export default function FlashCardList() {
-  const [showAnswer, setShowAnswer] = useState(false);
-
-  function toggleFlip() {
-    setShowAnswer(!showAnswer);
-  }
-
   return (
     <>
       <StyledHeading>Flip Cards List</StyledHeading>
       <StyledList>
         {flashcards.map(({ id, question, answer, collectionId }) => (
-          <StyledCard key={id} onClick={toggleFlip}>
-            <p>
-              {
-                collections.find((collection) => collectionId === collection.id)
-                  ?.title
-              }
-            </p>
-            {showAnswer ? (
-              <StyledAnswer>{answer}</StyledAnswer>
-            ) : (
-              <StyledQuestion>{question}</StyledQuestion>
-            )}
-          </StyledCard>
+          <FlashCard
+            key={id}
+            question={question}
+            answer={answer}
+            collectionId={collectionId}
+          />
         ))}
       </StyledList>
     </>
