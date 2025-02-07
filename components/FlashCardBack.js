ed991366c46184368cc3a8c7c5e5f7567c4f8e23
Sergii <96418850+ImMrSage@users.noreply.github.com>
2025-02-07 09:51:28 +0100

    delete unnecessary flashcardback comments and a function

--- components/FlashCardBack.js
+++ components/FlashCardBack.js
@@ -1,16 +1,10 @@
 import { collections } from "@/lib/data";
 import styled from "styled-components";
 
-// const CardBack = styled.div``;
-
 const StyledAnswer = styled.p`
   font-size: 1.75rem;
   font-weight: 600;
 `;
-
-function handleCorrect() {
-  // delete from Flip Cards List & add to archive
-}
 
 export default function FlashCardBack({ answer, collectionId }) {
   return (
