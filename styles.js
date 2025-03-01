b2ca4c392afed5dfbeab1eeca0c4d4a5fcd55788
denoerss <denise-albrecht@posteo.de>
2025-03-01 16:57:15 +0100

    Add first styles to global styles

--- styles.js
+++ styles.js
@@ -1,9 +1,22 @@
 import { createGlobalStyle } from "styled-components";
 import { Inter } from "next/font/google";
 
+// Fonts
 const inter = Inter({ subsets: ["latin"] });
-console.log(inter);
+
 export default createGlobalStyle`
+:root {
+  // Primary Colors
+  --black: #000000;
+  --white: #ffffff;
+  --light-grey:#cacaca;
+  
+  // Collection Colors
+
+  // Font-Sizes
+  // Font-Weights
+};
+
   *,
   *::before,
   *::after {
@@ -13,5 +26,9 @@ export default createGlobalStyle`
   body {
     margin: 0;
     font-family: ${inter.style.fontFamily}, system-ui, sans-serif;
+  }
+
+  h1 {
+    color: var(--black) ;
   }
 `;
